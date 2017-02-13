3bc6263e20043a93a1de37ee328da47a92a2e75b
Shumeyko Alexander Mihaylovich <AMShumeyko.SBT@sberbank.ru>
2017-02-13 17:04:39 +0300

    Добавил пример выборки данных

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,9 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import javax.persistence.Query;
 import javax.persistence.metamodel.Metamodel;
+import java.util.List;
 import java.util.Set;
 
 /**
@@ -27,13 +29,29 @@ public class Main
       Metamodel metamodel = session.getMetamodel();
       Set enSets = metamodel.getEntities();
   
+      /*
       //Создадим POJO сущность
-      TableNameEntity townEntity = new TableNameEntity(68784);
+      TableNameEntity townEntity = new TableNameEntity(902);
       //  и сохраним ее в БД
       session.beginTransaction();
       session.save(townEntity);
       session.getTransaction().commit();
-      session.close();
+      */
+      
+      session.beginTransaction();
+      //Создаем запрос
+      Query query = session.createQuery("FROM entities.TableNameEntity WHERE id = :id");
+      query.setParameter("id", 78); //Задаем переменные запроса
+      List<TableNameEntity> list1 = query.getResultList(); //Получить список результирующих сущностей
+      //Получить одну сущность, запрос должен возвращать немножесвенный результат
+      TableNameEntity tableNameEntity = (TableNameEntity)query.getSingleResult();
+      
+      //А тут вернем много сущностей
+      query = session.createQuery("FROM entities.TableNameEntity WHERE id != :id");
+      query.setParameter("id", 78);
+      List<TableNameEntity> list2 = query.getResultList();
+      
+      session.getTransaction().commit();
 
     }
     catch(Throwable th)
