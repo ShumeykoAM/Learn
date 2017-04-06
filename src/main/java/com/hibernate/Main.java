6f8f4596ccf9c0094aac4361873323cf89a611c7
kot <shumeyko.a.m@gmail.com>
2017-04-06 23:03:32 +0300

    Добавил пример Hibernate с маппингом в xml

--- src/main/java/com/hibernate/Main.java
+++ src/main/java/com/hibernate/Main.java
@@ -1,11 +1,45 @@
 package com.hibernate;
 
-/**
- * @author sbt-shumeyko-am
- * @ created 05.04.2017
- * @ $Author$
- * @ $Revision$
- */
+import com.hibernate.entities.Students;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import java.util.List;
+import javax.persistence.Query;
+
 public class Main
 {
+    public static void main(String[] args) {
+
+        try
+        {
+
+            ServiceRegistry serviceRegistry;
+            Configuration configuration = new Configuration();
+            configuration.configure();
+            SessionFactory sessionFactory = configuration.buildSessionFactory();
+            Session session = sessionFactory.openSession();
+
+            Students students = new Students();
+            students.setName("Имя4");
+            students.setId(8);
+
+            Transaction transaction = session.beginTransaction();
+            session.persist(students);
+            transaction.commit();
+
+            Query query = session.createNamedQuery("query.q1");
+            List list = query.getResultList();
+
+            session.close();
+            sessionFactory.close();
+        }
+        catch (Throwable e)
+        {
+            int f = 0;
+        }
+    }
 }
