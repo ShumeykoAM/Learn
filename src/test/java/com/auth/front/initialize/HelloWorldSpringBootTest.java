1198b2de6b72481512779259b04dc7fe151ec4a9
Shumeyko Alexander Mihaylovich <AMShumeyko.SBT@sberbank.ru>
2017-10-17 16:45:00 +0300

    ok

--- src/test/java/com/auth/front/initialize/HelloWorldSpringBootTest.java
+++ src/test/java/com/auth/front/initialize/HelloWorldSpringBootTest.java
@@ -1,9 +1,5 @@
 package com.auth.front.initialize;
 
-import org.junit.runner.RunWith;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
 /**
  * @author sbt-shumeyko-am
  * @ created 17.10.2017
@@ -11,8 +7,8 @@ import org.springframework.test.context.junit4.SpringRunner;
  * @ $Revision$
  */
 
-@RunWith(SpringRunner.class)
-@SpringBootTest
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
 public class HelloWorldSpringBootTest
 {
 
