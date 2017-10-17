2231ffb16c462788ef9b78a1fb0debfb555cd4cd
kot <shumeyko.a.m@gmail.com>
2017-10-17 20:04:28 +0300

    Вот так

---
+++ src/test/java/sbrf/esa2/autnnz/MainAppTest.java
@@ -0,0 +1,31 @@
+package sbrf.esa2.autnnz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+/**
+ * Тест инициализации Spring Boot
+ *
+ * @author sbt-shumeyko-am
+ * @ created 17.10.2017
+ * @ $Author$
+ * @ $Revision$
+ */
+public class MainAppTest
+{
+	@RunWith(SpringRunner.class)
+	@SpringBootTest
+	public class DemoApplicationTests
+	{
+		/**
+		 * TODO
+		 */
+		@Test
+		public void contextLoads()
+		{
+			
+		}
+	}
+}
