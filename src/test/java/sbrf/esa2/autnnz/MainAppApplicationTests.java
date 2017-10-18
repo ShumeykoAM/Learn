144aa969d30a47b5aa1f5a21aad07e4e6c30a804
Shumeyko Alexander Mihaylovich <AMShumeyko.SBT@sberbank.ru>
2017-10-18 09:44:27 +0300

    ok

---
+++ src/test/java/sbrf/esa2/autnnz/MainAppApplicationTests.java
@@ -0,0 +1,16 @@
+package sbrf.esa2.autnnz;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MainAppApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
