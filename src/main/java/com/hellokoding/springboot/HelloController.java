b7341d354ca4bb3b50d204972d1306e9d5bee6fb
kot <shumeyko.a.m@gmail.com>
2018-02-01 23:37:58 +0300

    Spring RestFull @JsonSerialize @JsonDeserialize поля POJO при биндинге в из Json

--- src/main/java/com/hellokoding/springboot/HelloController.java
+++ src/main/java/com/hellokoding/springboot/HelloController.java
@@ -1,10 +1,20 @@
 package com.hellokoding.springboot;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import org.springframework.http.MediaType;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.io.IOException;
 import javax.validation.Valid;
 import javax.validation.constraints.NotNull;
 
@@ -21,7 +31,7 @@ public class HelloController
 	public @ResponseBody Book welcomePage(@Valid @RequestBody Book p1, BindingResult result)
 	{
 
-		return new Book("dd");
+		return new Book("dd", 34);
 	}
 
 
@@ -38,13 +48,19 @@ public class HelloController
 		@NotNull
 		private String name;
 
+		@JsonProperty("c o d e")
+		@JsonSerialize(using = IntToStringSerializer.class)
+		@JsonDeserialize(using = StringToIntSerializer.class)
+		private Integer code;
+
 		public Book()
 		{
 		}
 
-		public Book(String name)
+		public Book(String name, Integer code)
 		{
 			this.name = name;
+			this.code = code;
 		}
 
 		public String getName()
@@ -55,6 +71,34 @@ public class HelloController
 		public void setName(String name)
 		{
 			this.name = name;
+		}
+
+		public Integer getCode()
+		{
+			return code;
+		}
+
+		public void setCode(Integer code)
+		{
+			this.code = code;
+		}
+
+		public static class StringToIntSerializer extends JsonDeserializer<Integer>
+		{
+			@Override
+			public Integer deserialize(JsonParser p, DeserializationContext ctxt) throws IOException, JsonProcessingException
+			{
+				return Integer.parseInt(p.getText());
+			}
+		}
+
+		public static class IntToStringSerializer extends JsonSerializer<Integer>
+		{
+			@Override
+			public void serialize(Integer value, JsonGenerator gen, SerializerProvider serializers) throws IOException, JsonProcessingException
+			{
+				gen.writeString(value.toString());
+			}
 		}
 	}
 }
