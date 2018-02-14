4e46a5616b0022d6197f9e6f46c1c71b44dd3c74
kot <shumeyko.a.m@gmail.com>
2018-02-14 22:40:16 +0300

    Пример ручного маршалинга Json

--- src/main/java/com/hellokoding/springboot/RestClient.java
+++ src/main/java/com/hellokoding/springboot/RestClient.java
@@ -1,5 +1,8 @@
 package com.hellokoding.springboot;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.http.*;
 import org.springframework.util.LinkedMultiValueMap;
 import org.springframework.util.MultiValueMap;
@@ -10,6 +13,8 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.Map;
+import java.util.TreeMap;
 import javax.validation.Valid;
 
 /**
@@ -32,7 +37,23 @@ public class RestClient
 	
 	public static void main(String args[])
 	{
-		String url = "http://localhost:8080/client";
+		CreateTransactionRequest request = new CreateTransactionRequest();
+		request.setOperationCode("opcode");
+		request.setDocument("doc".getBytes());
+		request.setParameters(new TreeMap<String, String>(){{put("p1", "v1"); put("p2", "v2");}});
+		ObjectMapper mapper = new ObjectMapper();
+		try
+		{
+			String m = mapper.writeValueAsString(request);
+			m = null;
+		}
+		catch (JsonProcessingException e)
+		{
+			e.printStackTrace();
+		}
+
+
+		String url = "http://localhost:8080/mj";
 		RestTemplate template = new RestTemplate();
 		HttpHeaders headers = new HttpHeaders();
 		headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
@@ -49,8 +70,50 @@ public class RestClient
 			token = e.getMessage();
 		}
 	}
-
 }
+
+class CreateTransactionRequest
+{
+	@JsonProperty("OperationCode")
+	private String operationCode;
+
+	@JsonProperty("Document")
+	private byte[] document;
+
+	@JsonProperty("Parameters")
+	private Map<String, String> parameters;
+
+	public String getOperationCode()
+	{
+		return operationCode;
+	}
+
+	public void setOperationCode(String operationCode)
+	{
+		this.operationCode = operationCode;
+	}
+
+	public byte[] getDocument()
+	{
+		return document;
+	}
+
+	public void setDocument(byte[] document)
+	{
+		this.document = document;
+	}
+
+	public Map<String, String> getParameters()
+	{
+		return parameters;
+	}
+
+	public void setParameters(Map<String, String> parameters)
+	{
+		this.parameters = parameters;
+	}
+}
+
 
 class Par
 {
