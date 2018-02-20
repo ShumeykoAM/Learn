97272e4a18195eac1e4c71ba667bb1358f6dd81f
kot <shumeyko.a.m@gmail.com>
2018-02-20 22:33:19 +0300

    Пример ручной конвертации из Pojo в Json

--- Spring/src/main/java/com/learn/rest/MyRestController.java
+++ Spring/src/main/java/com/learn/rest/MyRestController.java
@@ -1,5 +1,7 @@
 package com.learn.rest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.learn.rest.requests.JsonRequest;
 import com.learn.rest.responsies.JsonResponse;
 import org.springframework.http.HttpStatus;
@@ -8,6 +10,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Map;
+import java.util.TreeMap;
 import javax.validation.Valid;
 
 /**
@@ -38,6 +42,73 @@ public class MyRestController
 		result.hasErrors();
 		result.hasFieldErrors();
 
+		manualSerializeToJson();
+
 		return new ResponseEntity<JsonResponse>(new JsonResponse(false), HttpStatus.OK);
+	}
+
+	/**
+	 * Пример ручной конвертации из Pojo в Json
+	 */
+	private void manualSerializeToJson()
+	{
+		Request r = new Request();
+		Request.Value value = new Request.Value();
+		Map map = new TreeMap<String, String>(){{put("RefId", "Identif");}};
+		value.setValueResult(new Map[]{map});
+		r.setValue(value);
+
+		ObjectMapper mapper = new ObjectMapper();
+		try
+		{
+			String m = mapper.writeValueAsString(r);
+			m = null;
+		}
+		catch (JsonProcessingException e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
+
+class Request
+{
+	private String Result;
+
+	private Value Value;
+
+	public String getResult ()
+	{
+		return Result;
+	}
+
+	public void setResult (String Result)
+	{
+		this.Result = Result;
+	}
+
+	public Value getValue ()
+	{
+		return Value;
+	}
+
+	public void setValue (Value Value)
+	{
+		this.Value = Value;
+	}
+
+	public static class Value
+	{
+		private Map<String, String>[] ValueResult;
+
+		public Map<String, String>[] getValueResult()
+		{
+			return ValueResult;
+		}
+
+		public void setValueResult(Map<String, String>[] valueResult)
+		{
+			ValueResult = valueResult;
+		}
 	}
 }
