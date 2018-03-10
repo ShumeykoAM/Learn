9dc61a13453aaaa4bc6a24f00d1a201da076044d
kot <shumeyko.a.m@gmail.com>
2018-03-10 23:02:41 +0300

    Сделал сервисы для моей задачи по DSS, теперь можно перенаправить ответ от DSS на сервак, который дома запущен и от туда же узнать что было прислано. Добавил сохранение заголовка

--- Spring/src/main/java/com/learn/rest/RestPost.java
+++ Spring/src/main/java/com/learn/rest/RestPost.java
@@ -1,16 +1,20 @@
 package com.learn.rest;
 
+import com.google.common.base.Joiner;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.NoSuchElementException;
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 /**
- * TODO Description
+ * Сохранитель post запросов, для дальнейшего их изучения
  * @author Kot
  * @ created 09.03.2018
  * @ $Author$
@@ -24,13 +28,26 @@ public class RestPost
 	/**
 	 * Сюда можно слать любые POST запросы
 	 * @param request
-	 * @param result
+	 * @param httpRequest
 	 * @return
 	 */
 	@RequestMapping(value = {"/rest/post"}, method = RequestMethod.POST)
 	@ResponseBody
-	public ResponseEntity<String> restPost(@Valid @RequestBody String request, BindingResult result)
+	public ResponseEntity<String> restPost(@Valid @RequestBody String request, HttpServletRequest httpRequest)
 	{
+		List<String> headers = new ArrayList<>();
+		try
+		{
+			Enumeration<String> headerNames = httpRequest.getHeaderNames();
+			do
+			{
+				String headerName = headerNames.nextElement();
+				headers.add("name:     " + headerName + "\n  value:  " + httpRequest.getHeader(headerName));
+			}
+			while (true);
+		}
+		catch (NoSuchElementException e){   }
+		request += "\n\n\n" + Joiner.on('\n').join(headers);
 		requests.add(request);
 		return new ResponseEntity<String>("", HttpStatus.OK);
 	}
@@ -59,13 +76,12 @@ public class RestPost
 		String s = "Наверное запросов еще не было, или какая то другая ошибка.";
 		try
 		{
-			if(request == -1)
-				request = requests.size()-1;
+			if (request == -1)
+				request = requests.size() - 1;
 			s = requests.get(request);
 		}
 		catch (Throwable th)
 		{		}
 		return new ResponseEntity<String>(s, HttpStatus.OK);
 	}
-
 }
