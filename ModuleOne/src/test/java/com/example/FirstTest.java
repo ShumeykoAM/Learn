8defb2503d1b04ef5a8043b412dd5dae7f1a5ea1
shumeyko-am <shumeyko.a.m@gmail.com>
2019-08-02 21:19:14 +0300

    Создал gradle проект, подключил библиотеки junit и mokito, и написал простейший тест

---
+++ ModuleOne/src/test/java/com/example/FirstTest.java
@@ -0,0 +1,22 @@
+package com.example;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+/**
+ * TODO Description
+ * @author kot
+ * @ created 02.08.2019
+ * @ $Author$
+ * @ $Revision$
+ */
+public class FirstTest
+{
+
+	@Test
+	public void oneTest()
+	{
+		assertEquals("Совпал", "Совпал");
+	}
+}
